3b53bed9ec71f1c661c2a1f8229ddaeb06eede0e
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-09-14 15:50:24 +0530

    Add dropdown wise paginate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaginationComponent } from './pagination/pagination.component';
@@ -15,7 +15,8 @@ import {NgxPaginationModule} from 'ngx-pagination'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxPaginationModule
+    NgxPaginationModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
